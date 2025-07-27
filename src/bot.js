60595da01d789a87b2e5b5a67f0eea12fc31ef7a
Nikitos1407 <nikitos14021997@gmail.com>
2025-07-27 14:41:29 +0400

    Add files via upload

---
+++ src/bot.js
@@ -0,0 +1,24 @@
+const { Telegraf, Markup, session } = require('telegraf');
+const { BOT_TOKEN, OWNER_ID } = require('./config');
+const { userStates, userHistory } = require('./utils/storage');
+const { isValidDate, parseDate, getRentalDays } = require('./utils/date');
+const tools = require('./data/tools');
+const { mainMenu } = require('./menus');
+
+if (!BOT_TOKEN || !OWNER_ID) {
+  console.error('Ошибка: BOT_TOKEN или OWNER_ID не задан.');
+  process.exit(1);
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+bot.use(session());
+
+// Команды
+require('./commands/start')(bot, mainMenu);
+require('./commands/menu')(bot, mainMenu);
+require('./commands/instruments')(bot, tools);
+require('./commands/rental')(bot, tools, userStates, userHistory, isValidDate, parseDate, getRentalDays);
+require('./commands/history')(bot, userHistory);
+require('./commands/misc')(bot);
+
+module.exports = bot;
