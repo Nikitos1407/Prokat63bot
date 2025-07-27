8c32ab7430089e0785f14e76ae9ce4a98c40da47
Nikitos1407 <nikitos14021997@gmail.com>
2025-07-27 22:24:15 +0400

    Update instruments.js

--- src/commands/instruments.js
+++ src/commands/instruments.js
@@ -1,20 +1,16 @@
 const { Markup } = require('telegraf');
-
 module.exports = (bot, tools) => {
   bot.hears('📋 Список инструментов', ctx => {
     const buttons = tools.map(t => [Markup.button.callback(`${t.name} — ${t.price}₽`, t.id)]);
     ctx.reply('Выберите инструмент:', Markup.inlineKeyboard(buttons));
   });
-
   tools.forEach(tool => {
     bot.action(tool.id, async ctx => {
       await ctx.answerCbQuery();
       await ctx.replyWithPhoto(tool.photo, {
         caption:
 `🛠 *${tool.name}*
-
 ${tool.description}
-
 💰 *Цена:* ${tool.price} ₽/сутки
 🔐 *Залог:* ${tool.deposit} ₽`,
         parse_mode: 'Markdown',
