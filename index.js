39906e856f6002394e1b05aa20d96e8f787c8df7
Nikitos1407 <nikitos14021997@gmail.com>
2025-08-03 22:50:01 +0400

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const bot = require('./src/bot');
-require('./src/server');
-
-bot.launch()
-  .then(() => console.log('🤖 Бот запущен (polling)'))
-  .catch(err => console.error('❌ Ошибка запуска бота:', err));
